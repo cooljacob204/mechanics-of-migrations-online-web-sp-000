39018c6f45e55fee77e7647e46225da9798ae752
Jacob Feldman <jacobgfeld@gmail.com>
2019-02-12 19:43:07 +0000

    Automatically backed up by Learn

--- artist.rb
+++ artist.rb
@@ -1,9 +1,9 @@
 class Artist < ActiveRecord::Base
   attr_reader :name, :genre, :age, :hometown
   def initialize(**attr)
-    @name = name
-    @genre = genre
-    @age = age
-    @hometown = hometown
+    @name = [attr]name
+    @genre = [attr]genre
+    @age = [attr]age
+    @hometown = [attr]hometown
   end
 end
